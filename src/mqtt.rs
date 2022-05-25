409ce328c2a475b671af0bf6e8fa63218644db1a
Dan Nixon <dan@dan-nixon.com>
2022-05-25 12:36:08 +0100

    Reconnect to broker when disconnected

--- src/mqtt.rs
+++ src/mqtt.rs
@@ -1,7 +1,7 @@
 use super::{Cli, Event};
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use paho_mqtt::{AsyncClient, ConnectOptionsBuilder, CreateOptionsBuilder, Message};
-use std::env;
+use std::{env, time::Duration};
 use tokio::{sync::broadcast::Sender, task::JoinHandle};
 
 pub(crate) fn run(tx: Sender<Event>, args: &Cli) -> Result<JoinHandle<()>> {
@@ -36,14 +36,37 @@ pub(crate) fn run(tx: Sender<Event>, args: &Cli) -> Result<JoinHandle<()>> {
                     match client.try_publish(Message::new(msg.topic, msg.message, qos)) {
                         Ok(delivery_token) => {
                             if let Err(e) = delivery_token.wait() {
-                                log::error! {"Error sending message: {}", e};
+                                log::error!("Error sending message: {}", e);
                             }
                         }
-                        Err(e) => log::error! {"Error creating/queuing the message: {}", e},
+                        Err(e) => {
+                            log::error!("Error creating/queuing message: {}", e);
+                            if let Err(e) = try_reconnect(&client).await {
+                                log::error!("Failed to reconnect: {}", e);
+                                tx.send(Event::Exit).unwrap();
+                            }
+                        }
                     }
                 }
                 _ => {}
             }
         }
     }))
+}
+
+async fn try_reconnect(c: &AsyncClient) -> Result<()> {
+    for i in 0..10 {
+        log::info!("Attempting reconnection {}...", i);
+        match c.reconnect().await {
+            Ok(_) => {
+                log::info!("Reconnection successful");
+                return Ok(());
+            }
+            Err(e) => {
+                log::error!("Reconnection failed: {}", e);
+            }
+        }
+        tokio::time::sleep(Duration::from_secs(1)).await;
+    }
+    Err(anyhow!("Failed to reconnect to broker"))
 }
