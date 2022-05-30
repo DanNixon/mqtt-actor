976debd64cd907a686c6e3a5504504b4c64d6e69
Dan Nixon <dan@dan-nixon.com>
2022-05-30 10:29:38 +0100

    Improve MQTT disconnection handling

--- src/mqtt.rs
+++ src/mqtt.rs
@@ -1,7 +1,9 @@
 use super::{Cli, Event};
-use anyhow::{anyhow, Result};
-use paho_mqtt::{AsyncClient, ConnectOptionsBuilder, CreateOptionsBuilder, Message};
-use std::{env, time::Duration};
+use anyhow::Result;
+use paho_mqtt::{
+    AsyncClient, ConnectOptionsBuilder, CreateOptionsBuilder, Message, PersistenceType,
+};
+use std::time::Duration;
 use tokio::{sync::broadcast::Sender, task::JoinHandle};
 
 pub(crate) fn run(tx: Sender<Event>, args: &Cli) -> Result<JoinHandle<()>> {
@@ -9,18 +11,29 @@ pub(crate) fn run(tx: Sender<Event>, args: &Cli) -> Result<JoinHandle<()>> {
         CreateOptionsBuilder::new()
             .server_uri(&args.mqtt_broker)
             .client_id(&args.mqtt_client_id)
-            .persistence(env::temp_dir())
+            .persistence(PersistenceType::None)
             .finalize(),
     )?;
 
-    client
+    client.set_connected_callback(|_| {
+        log::info!("Connected to broker");
+    });
+
+    let response = client
         .connect(
             ConnectOptionsBuilder::new()
+                .automatic_reconnect(Duration::from_secs(1), Duration::from_secs(5))
+                .keep_alive_interval(Duration::from_secs(5))
                 .user_name(&args.mqtt_username)
                 .password(&args.mqtt_password)
                 .finalize(),
         )
         .wait()?;
+
+    log::info!(
+        "Using MQTT version {}",
+        response.connect_response().unwrap().mqtt_version
+    );
 
     let mut rx = tx.subscribe();
     let qos = args.mqtt_qos;
@@ -41,10 +54,6 @@ pub(crate) fn run(tx: Sender<Event>, args: &Cli) -> Result<JoinHandle<()>> {
                         }
                         Err(e) => {
                             log::error!("Error creating/queuing message: {}", e);
-                            if let Err(e) = try_reconnect(&client).await {
-                                log::error!("Failed to reconnect: {}", e);
-                                tx.send(Event::Exit).unwrap();
-                            }
                         }
                     }
                 }
@@ -52,21 +61,4 @@ pub(crate) fn run(tx: Sender<Event>, args: &Cli) -> Result<JoinHandle<()>> {
             }
         }
     }))
-}
-
-async fn try_reconnect(c: &AsyncClient) -> Result<()> {
-    for i in 0..10 {
-        log::info!("Attempting reconnection {}...", i);
-        match c.reconnect().await {
-            Ok(_) => {
-                log::info!("Reconnection successful");
-                return Ok(());
-            }
-            Err(e) => {
-                log::error!("Reconnection failed: {}", e);
-            }
-        }
-        tokio::time::sleep(Duration::from_secs(1)).await;
-    }
-    Err(anyhow!("Failed to reconnect to broker"))
 }
